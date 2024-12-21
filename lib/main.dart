4d1212f1d4c72bcbd1138c83401bffd27fe746e4
Dukeoohhh <pasut5345@gmail.com>
2024-12-21 16:00:03 +0700

    Create containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demoproject/homepage.dart';
+import 'package:myapp/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,11 +13,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Demo",
+              style: TextStyle(color: Colors.amberAccent, fontSize: 30, fontWeight: FontWeight.bold)),
+          leading: const Icon(Icons.add),
+          backgroundColor: Colors.cyan,
+        ),
+        body: Container(
+          height: 100,
+          width: 100,
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            border: Border.all(
+              color: const Color.fromARGB(0, 0, 0, 0),
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Text(
+            "Hello World",
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Hello");
+          },
+          child: const Icon(Icons.phone),
+        ),
+      ),
     );
   }
 }
